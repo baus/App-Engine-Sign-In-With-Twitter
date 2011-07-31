3b88c6aa5d1f14522fd80556e24b6960a117aeb5
Chris Baus <christopher@baus.net>
2011-07-31 11:06:09 -0700

    Added "Sign in with Twitter" icon instead of using text link.

--- twitteroauthkeys.py
+++ twitteroauthkeys.py
@@ -5,5 +5,5 @@ Contains the Twitter oauth keys for your application.
 
 You have to register your app with twitter here: https://dev.twitter.com/apps
 """
-TWITTER_CONSUMER_KEY = "xxx"    #replace with your consumer key
-TWITTER_CONSUMER_SECRET = "xxx" #replace with your consumer secret
+TWITTER_CONSUMER_KEY = "XXX"    #replace with your consumer key
+TWITTER_CONSUMER_SECRET = "XXX" #replace with your consumer secret
