c65e3042da4b57c097760e86f77eee259dffbcac
Christopher Baus <christohper@baus.net>
2013-01-07 17:59:29 -0800

    working creating form to edit OAuthService properties

--- oauthclient/forms.py
+++ oauthclient/forms.py
@@ -1,4 +1,5 @@
 import django.forms
+import datastore
 
 class ServiceForm(django.forms.Form):
     display_name = django.forms.CharField()
@@ -8,3 +9,18 @@ class ServiceForm(django.forms.Form):
     authorize_url = django.forms.URLField(initial='http://')
     access_token_url = django.forms.URLField(initial='http://')
 
+def create_service_formset():
+    initial_values = []
+    for service in datastore.OAuthService.all():
+        service_initial_values = {}
+        service_initial_values["display_name"] = service.display_name
+        service_initial_values["consumer_secret"] = service.consumer_secret
+        service_initial_values["consumer_key"] = service.consumer_key
+        service_initial_values["request_token_url"] = service.request_token_url
+        service_initial_values["authorize_url"] = service.authorize_url
+        service_initial_values["access_token_url"] = service.access_token_url
+        initial_values.add(service_initial_values)
+    
+    ServiceFormset = formset_factory(ServiceForm)
+    return ServiceFormset(initial = service_initial_values)
+    
